db48b03529cb6b703366c577b19f0ffefcee704d
victor staven <vista17@student.sdu.dk>
2019-03-04 17:13:13 +0100

    Updated sw1.c and sw1.h for code standard

--- src/sw1.c
+++ src/sw1.c
@@ -1,11 +1,40 @@
+/*****************************************************************************
+* University of Southern Denmark
+* Embedded Programming (EMP)
+*
+* MODULENAME.: sw1.c
+*
+* PROJECT....: Ass1
+*
+* DESCRIPTION: See module specification file (.h-file).
+*
+* Change Log:
+*****************************************************************************
+* Date    Id    Change
+* YYMMDD
+* --------------------
+* 190302  KA    Module created.
+*
+*****************************************************************************/
+
+/***************************** Include files *******************************/
 #include <stdint.h>
 #include <stdbool.h>
 #include "tm4c123gh6pm.h"
 #include "sw1.h"
-
+/*****************************    Defines    *******************************/
 #define SW1 0x10
+/*****************************   Constants   *******************************/
 
+/*****************************   Variables   *******************************/
+
+/*****************************   Functions   *******************************/
 void sw1_setup()
+/*****************************************************************************
+*   Input    : Nothing
+*   Output   : Nothing
+*   Function : To set up the necessary pins and values for sw1
+******************************************************************************/
 {
 	SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R5;
 	while( (SYSCTL_PRGPIO_R & SYSCTL_PRGPIO_R5) == 0);
@@ -17,9 +46,15 @@ void sw1_setup()
 }
 
 bool sw1_read()
+/*****************************************************************************
+*   Input    : Nothing
+*   Output   : The state, is the switch on, then return TRUE, otherwise return FALSE.
+*   Function : To figure out rather or not the switch is on or off.
+******************************************************************************/
 {
-	if( GPIO_PORTF_DATA_R & SW1)
+	if ( GPIO_PORTF_DATA_R & SW1 )
 		return false;
 	else
-		return true;	
+		return true;
 }
+/****************************** End Of Module *******************************/
